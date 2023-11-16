cbc5636c0a49bee9a6b12961a4916956f99ffdb5
Majid Ali <itsmajidali129@gmail.com>
2023-11-16 23:57:33 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+// https://github.com/majidali129/task-management-app.git
